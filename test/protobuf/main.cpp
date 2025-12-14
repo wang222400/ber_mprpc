560748c3c40d46f3697912cce61028e95a3e5ffa
wang222400 <wang222400@gmail.com>
2025-12-14 16:33:01 +0800

    protobu的应用测试

---
+++ test/protobuf/main.cpp
@@ -0,0 +1,29 @@
+#include "test.pb.h"
+#include <iostream>
+#include <string>
+
+using namespace fixbug;
+
+int main()
+{
+    //封装login请求对象的数据
+    LoginRequest req;
+    req.set_name("zhang san");
+    req.set_pwd("123456");
+    std::string send_str;
+
+    //对象数据序列化
+    if(req.SerializeToString(&send_str)){
+        std::cout<<send_str.c_str()<<std::endl;
+    }
+
+    //从send_str反序列化一个login请求对象
+    LoginRequest reqB;
+    if(reqB.ParseFromString(send_str)){
+        std::cout << reqB.name() << std::endl;
+        std::cout << reqB.pwd() << std::endl;
+    }
+
+
+    return 0;
+}
